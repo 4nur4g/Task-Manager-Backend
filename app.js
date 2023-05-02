74e3f6e398e5d140a445ecec3762d58a13de5962
Anurag Pradhan <anuragp936@gmail.com>
2023-05-02 13:45:31 +0530

    Almost complete project

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ require('dotenv').config()
 
 // middleware
 
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-})
 
 app.use('/api/v1/tasks', tasks);
 const port = process.env.PORT || 3000; // default port to listen
--- controller/tasks.js
+++ controller/tasks.js
@@ -18,17 +18,50 @@ const createTask = async (req, res) => {
     }
 }
 
-const getTask = (req, res) => {
-    res.json({id: req.params.id});
-}
-const updateTask = (req, res) => {
-    res.send('Update item');
+const getTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOne({_id: taskID});
+        if(!task) {
+            return res.status(404).json({error: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('Delete item');
+const deleteTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id: taskID});
+        if(!task) {
+            return res.status(404).json({msg: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    }
+    catch (error) {
+        res.status(404).json({error: error.message});
+    }
 }
 
+const updateTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        if(!task) {
+            return res.status(404).json({error: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({task});
+    }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
 module.exports = {
     getAllTasks,
     createTask,
